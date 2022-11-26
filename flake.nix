13b3a3ff3a3efc3ac98143e8011f3c4b86073fc8
Emory Merryman <emory.merryman@gmail.com>
2022-11-26 08:02:14 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
 		  (
 		    nixpkgs : scripts :
 		      let
-		        _ = utilsx.visit { list = track : builtins.typeOf track ; string = track : track.processed ; } ( scripts structure ) ;
+		        _ = utilsx.visit { } ( scripts structure ) ;
 		        pkgs = builtins.getAttr system nixpkgs.legacyPackages ;	  
 			structure =		       			      
 			  {
