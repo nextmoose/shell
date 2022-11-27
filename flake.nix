7772d8148ff33ce75f38a81600653ae5d7da952c
Emory Merryman <emory.merryman@gmail.com>
2022-11-27 07:51:13 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
                                       {
                                         success = _utils.visit {
 					  list = track : builtins.trace "builtins.all ( x : x ) track.processed" true ;
-					  set = track : builtins.trace " SET ${ builtins.typeOf track.processed }" true ;
+					  set = track : builtins.trace " SET ${ builtins.concatStringsSep " , " ( builtins.attrNames track.value ) }" true ;
 					  string = set : track : track.processed ;
 					  } ( structure 0 ) ;
                                         value = eye ;
