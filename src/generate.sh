7be845111ce4a9e9d694c4660d5645f7483b5a42
Emory Merryman <emory.merryman@gmail.com>
2022-12-03 11:48:15 -0500

    TESTED

--- src/generate.sh
+++ src/generate.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-while [ ${ _utils.bash-variable "#" } -gt 0 ]
+while [ "${#}" -gt 0 ]
 do
-    case ${ _utils.bash-variable "1" } in
+    case "${1}" in
         --resource-directory)
-            RESOURCE_DIRECTORY=${ _utils.bash-variable "2" } ;
+            RESOURCE_DIRECTORY="${2}" ;
             shift 2 &&
                 break
             ;;
         --hook)
-            HOOK=${ _utils.bash-variable "2" } &&
+            HOOK="${2}" &&
                 shift 2 &&
                 break
             ;;
         --inputs)
-            INPUTS=${ _utils.bash-variable "2" } &&
+            INPUTS="${2}" &&
                 shift 2 &&
                 break
             ;;
@@ -25,11 +25,11 @@ do
             ;;
     esac
 done &&
-    SCRIPT_DIRECTORY=$( ${ pkgs.mktemp }/bin/mktemp --directory ) &&
-    cd ${ _utils.bash-variable "SCRIPT_DIRECTORY" } &&
-    ${ pkgs.nix }/bin/nix flake init &&
-    ${ pkgs.coreutils }/bin/cp ./src/flake.nix flake.nix &&
-    ${ pkgs.coreutils }/bin/echo "${ _utils.bash-variable "hook" }" > hook.nix &&
-    ${ pkgs.coreutils }/bin/echo "${ _utils.bash-variable "inputs" }" > inputs.nix &&
-    ${ pkgs.coreutils }/bin/echo "${ _utils.bash-variable "inputs" }" > scripts.nix &&
-    ${ pkgs.coreutils }/bin/chmode 0400 flake.nix hook.nix inputs.txt scripts.tex
+    SCRIPT_DIRECTORY=$( ${MKTEMP}/bin/mktemp --directory ) &&
+    cd ${SCRIPT_DIRECTORY} &&
+    ${NIX}/bin/nix flake init &&
+    ${COREUTILS}/bin/cp ./src/flake.nix flake.nix &&
+    ${COREUTILS}/bin/echo "${ _utils.bash-variable "hook" }" > hook.nix &&
+    ${COREUTILS}/bin/echo "${ _utils.bash-variable "inputs" }" > inputs.nix &&
+    ${COREUTILS}/bin/echo "${ _utils.bash-variable "inputs" }" > scripts.nix &&
+    ${COREUTILS}/bin/chmod 0400 flake.nix hook.nix inputs.txt scripts.tex
