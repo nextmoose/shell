baaf9963710059fa2690811d126a915c6939fc5e
Emory Merryman <emory.merryman@gmail.com>
2022-12-10 06:16:00 -0500

    TESTED

--- src/flake.nix
+++ src/flake.nix
@@ -14,6 +14,8 @@
 		  let
 		    pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
 		    scripts = builtins.import scripts.tex ;
+		    structure-directory = import ./structure-directory.nix ;
+		    token = import ./token.nix ;
 		      in pkgs.mkShell
 		        {
 			  buildInputs = builtins.attrValues ( builtins.map ( name : value : pkgs.writeShellScriptBin name value ; ) ( builtins.import ./inputs.nix scripts ) ;
