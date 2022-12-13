ce50f609b767401a3c8f659b61bde935b02c626e
Emory Merryman <emory.merryman@gmail.com>
2022-12-12 22:37:36 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -103,6 +103,7 @@
                                       exec ${ n.log }<>${ _utils.bash-variable v.log }/lock &&
                                       ${ pkgs.flock }/bin/flock -n ${ n.log } &&
                                       ${ if builtins.replaceStrings [ v.temporary ] [ "" ] string == string then "${ pkgs.coreutils }/bin/true" else temporary } &&
+				      # ${ v.temporary }
                                       ${ pkgs.writeShellScriptBin "script" string }/bin/script \
                                         > >( ${ pkgs.moreutils }/bin/pee "${ pkgs.moreutils }/bin/ts %Y-%m-%d-%H-%M-%S > ${ _utils.bash-variable v.log }/out 2> /dev/null" "${ pkgs.coreutils }/bin/tee > /dev/stdout" ) \
                                         2> >( ${ pkgs.moreutils }/bin/pee "${ pkgs.moreutils }/bin/ts %Y-%m-%d-%H-%M-%S > ${ _utils.bash-variable v.log }/err 2> /dev/null" "${ pkgs.coreutils }/bin/tee > /dev/stderr" )
