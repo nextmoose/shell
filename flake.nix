e3c81038abd979e6107299d186ea981ca56053b3
Emory Merryman <emory.merryman@gmail.com>
2022-12-14 21:34:25 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
 				  do
 				    INDEX=$(( 200 + ${ _utils.bash-variable "I" } )) &&
 				    DIRECTORY=${ _utils.bash-variable "ARRAY[${ _utils.bash-variable "I" }]" } &&
-				    ${ pkgs.coreutils }/bin/echo ${ _utils.bash-variable "INDEX" } ${ _utils.bash-variable "DIRECTORY" } &&
+				    ${ pkgs.coreutils }/bin/echo WTF ${ _utils.bash-variable "INDEX" } ${ _utils.bash-variable "DIRECTORY" } &&
 				    exec ${ _utils.bash-variable "INDEX" }<>${ _utils.bash-variable "DIRECTORY" }/lock &&
 				    ${ pkgs.flock }/bin/flock -x -w 1 ${ _utils.bash-variable "INDEX" } &&
 				    ${ pkgs.coreutils }/bin/rm ${ _utils.bash-variable "DIRECTORY" }/lock
