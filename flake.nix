17dfcce17f90e4794a1811edb1a8a2452f419f2c
Emory Merryman <emory.merryman@gmail.com>
2022-12-15 12:30:01 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -161,6 +161,7 @@
                                         then
                                           exec ${ numbers.temporaries }<>${ structure-directory }/temporary/lock &&
                                           ${ pkgs.flock }/bin/flock -s ${ numbers.temporaries } &&
+                                          ${ pkgs.coreutils }/bin/echo ${ pkgs.findutils }/bin/find ${ _utils.bash-variable variables.temporary } -type f -exec ${ pkgs.coreutils }/bin/shred --force --remove {} \; >> ${ structure-directory }/commands.txt &&
                                           # ${ pkgs.findutils }/bin/find ${ _utils.bash-variable variables.temporary } -type f -exec ${ pkgs.coreutils }/bin/shred --force --remove {} \; &&
                                           # ${ pkgs.coreutils }/bin/rm --recursive --force ${ _utils.bash-variable variables.temporary }
                                           ${ pkgs.coreutils }/bin/true
