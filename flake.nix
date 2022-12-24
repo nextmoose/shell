dfb19503088a562f38f8dd99e123cb03866efec6
Emory Merryman <emory.merryman@gmail.com>
2022-12-24 15:18:16 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
                                                   ${ process track.reduced } &&
                                                   ${ temporary track.reduced } &&
                                                   ${ pkgs.writeShellScriptBin "script" ( _utils.strip track.reduced ) }/bin/script \
-                                                    > >( ${ pkgs.moreutils }/bin/pee "${ pkgs.moreutils }/bin/ts %Y-%m-%d-%H-%M-%S > ${ _utils.bash-variable variables.script.log }/out 2> /dev/null" "${ pkgs.coreutils }/bin/tee > /dev/stdout" )
+                                                    > >( ${ pkgs.moreutils }/bin/pee "${ pkgs.moreutils }/bin/ts %Y-%m-%d-%H-%M-%S > ${ _utils.bash-variable variables.script.log }/out 2> /dev/null" "${ pkgs.coreutils }/bin/tee > /dev/stdout" ) \
                                                     2> >( ${ pkgs.moreutils }/bin/pee "${ pkgs.moreutils }/bin/ts %Y-%m-%d-%H-%M-%S > ${ _utils.bash-variable variables.script.log }/err 2> /dev/null" "${ pkgs.coreutils }/bin/tee > /dev/stderr" ) &&
                                                   if [ -f ${ _utils.bash-variable variables.script.log }/err ]
                                                   then
