b90a23965408eb79e2b38756b7b47b6cdb098552
Emory Merryman <emory.merryman@gmail.com>
2022-12-28 07:24:08 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -282,7 +282,7 @@
                                                               cleanup ( )
                                                               {
 							        ${ pkgs.coreutils }/bin/echo 9a4e5cb1-8a21-4000-af73-9f1b41c26b2b 2726ad25-c5e7-4e6b-8858-873ed250b86b >> ${ structure-directory }/afa82912-e555-46ea-b5be-a178721b367e &&
-							        TIME= $( ${ pkgs.coreutils }/bin/echo \
+							        TIME=$( ${ pkgs.coreutils }/bin/echo \
                                                                   ${ pkgs.coreutils }/bin/nice \
                                                                     --adjustment 19 \
                                                                     ${ pkgs.writeShellScriptBin "cleanup" ( program cleanup ) }/bin/cleanup ${ _utils.bash-variable "LINK_DIRECTORY" } ${ _utils.bash-variable "RESOURCE_DIRECTORY" } | ${ at } now 2> >( ${ pkgs.coreutils }/bin/tail --lines 1 ) ) &&
