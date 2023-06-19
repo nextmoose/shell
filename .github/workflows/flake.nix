76f6d5c439f8fd11119bcb386071c741d4416d4d
Emory Merryman <emory.merryman@gmail.com>
2023-06-19 14:34:55 -0400

    restart

--- .github/workflows/flake.nix
+++ .github/workflows/flake.nix
@@ -1,11 +1,11 @@
   {
-    inputs = { flake-utils.url = "github:numtide/flake-utils" ; shell.url = "/home/emory/Desktop/structure/flakes/shell" ; } ;
+    inputs = { flake-utils.url = "github:numtide/flake-utils" ; shell.url = "/home/runner/work/shell/shell" ; } ;
     outputs =
       { flake-utils , shell , self } :
         shell.lib
           {
             hook = scripts : scripts.test.entry ;
             inputs = scripts : [ scripts.test.test-resource ] ;
-            structure-directory = "/home/emory/structure" ;
+            structure-directory = "/home/runner/work/_temp" ;
           } ;
   }
