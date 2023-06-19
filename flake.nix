af468418a84ce4903d89d3e4b6bc3b1177af6550
Emory Merryman <emory.merryman@gmail.com>
2023-06-19 14:57:22 -0400

    restart

--- flake.nix
+++ flake.nix
@@ -3,11 +3,10 @@
       {
         flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
         nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843"  ;
-        unique.url = "/home/emory/Desktop/structure/flakes/unique" ;
         visit.url = "/home/emory/Desktop/structure/flakes/visit" ;
       } ;
     outputs =
-      { flake-utils , nixpkgs , self , unique , visit } :
+      { flake-utils , nixpkgs , self , visit } :
         {
           lib =
             let
@@ -16,7 +15,7 @@
                   bash-variable = import ./bash-variable.nix ;
                   nixpkgs = nixpkgs ;
                   strip = import ./strip.nix ;
-                  unique = unique.lib { } ;
+                  unique = import ./unique.nix ;
                   visit = visit.lib { } ;
                 } ;
               in
