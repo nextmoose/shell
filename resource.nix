58f9d47b5501ae3976f5cb714348d9b6dbd0868d
Emory Merryman <emory.merryman@gmail.com>
2023-06-21 10:31:06 -0400

    try this

--- resource.nix
+++ resource.nix
@@ -12,12 +12,12 @@
       # This is a ${ type }
       ${ if make-directory then "${ coreutils }/bin/mkdir ${ structure-directory }/resource/${ bash-variable "HASH" }/resource" else "# NO NEED TO MAKE DIRECTORY" } &&
       ${ if make-directory then "cd ${ structure-directory }/resource/${ bash-variable "HASH" }/resource" else "# NO NEED TO cd" } &&
-      ${ coreutils }/bin/ln --symbolic ${ init } ${ structure-directory }/resource/${ bash-variable "HASH" }/init.sh &&
-      ${ structure-directory }/resource/${ bash-variable "HASH" }/init.sh ${ structure-directory }/resource/${ bash-variable "HASH" }/resource ${ if use-output then "> ${ structure-directory }/resource/${ bash-variable "HASH" }/resource" else "" } &&
-      ${ coreutils }/bin/chmod ${ permissions } ${ structure-directory }/resource/${ bash-variable "HASH" }/resource &&
+      ${ if builtins.typeOf init == "bool" then "# init" else "${ coreutils }/bin/ln --symbolic ${ init } ${ structure-directory }/resource/${ bash-variable "HASH" }/init.sh" } &&
+      ${ if builtins.typeOf init == "bool" then "# init" else "${ structure-directory }/resource/${ bash-variable "HASH" }/init.sh ${ structure-directory }/resource/${ bash-variable "HASH" }/resource ${ if use-output then "> ${ structure-directory }/resource/${ bash-variable "HASH" }/resource" else "" }" } &&
+      ${ if builtins.typeOf init == "bool" then "# init" else "${ coreutils }/bin/chmod ${ permissions } ${ structure-directory }/resource/${ bash-variable "HASH" }/resource" } &&
       ${ if builtins.typeOf release == "bool" then "# NO RELEASE" else "${ coreutils }/bin/ln --symbolic ${ release } ${ structure-directory }/resource/${ bash-variable "HASH" }/release.sh" } &&
-      ${ coreutils }/bin/touch ${ structure-directory }/resource/${ bash-variable "HASH" }/make-directory &&
-      ${ coreutils }/bin/chmod 0400 ${ structure-directory }/resource/${ bash-variable "HASH" }/make-directory &&
+      ${ if make-directory then "${ coreutils }/bin/touch ${ structure-directory }/resource/${ bash-variable "HASH" }/make-directory" else "# make-directory" } &&
+      ${ if make-directory then "${ coreutils }/bin/chmod 0400 ${ structure-directory }/resource/${ bash-variable "HASH" }/make-directory" else "# make-directory" } &&
       ${ coreutils }/bin/touch ${ structure-directory }/resource/${ bash-variable "HASH" }/exists &&
       ${ coreutils }/bin/chmod 0400 ${ structure-directory }/resource/${ bash-variable "HASH" }/exists
     fi &&
