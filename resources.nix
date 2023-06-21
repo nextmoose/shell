f5ed4ad39134230be7ee301bac71244781320a5e
Emory Merryman <emory.merryman@gmail.com>
2023-06-20 21:20:44 -0400

    do not show resource mine

--- resources.nix
+++ resources.nix
@@ -16,7 +16,7 @@
         lock = resource : resource { init = scripts : scripts.touch ; salt = 60 * 60 * 24 * 7 ; show = false ; permissions = 0600 ; } ;
       } ;
     mine = resource : resource { init = scripts : scripts.name ; salt = 1 ; show = true ; use-output = true ; } ;
-    name = resource : resource { init = scripts : scripts.name ; show = true ; use-output = true ; } ;
+    name = resource : resource { init = scripts : scripts.name ; show = false ; use-output = true ; } ;
     test =
       {
         locks =
