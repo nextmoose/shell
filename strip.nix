a6a65d8dc85a3ef736581c3b263ee739cd279e31
Emory Merryman <emory.merryman@gmail.com>
2023-08-06 17:38:00 -0400

    restart

--- strip.nix
+++ strip.nix
@@ -1,18 +1,16 @@
 let
   strip =
-    string :
+    expression :
       let
-        append = builtins.substring 1 m string ;
-        is-appended = is-whitespace ( builtins.substring m 1 string ) ;
-        is-prepended = is-whitespace ( builtins.substring 0 1 string ) ;
-        is-whitespace = character : builtins.any ( w : w == character ) whitespace ;
-        n = builtins.stringLength string ;
-        m = n - 1 ;
-        prepend = builtins.substring 0 m string ;
-        whitespace = [ " " "\t" "\n" "\r" "\v" ] ;
-        in
-          if builtins.typeOf string == "string" && n > 0 then
-            if is-prepended then strip append else if is-appended then strip prepend else string
-            else if n == 0 then ""
-        else builtins.throw "668ec04c-a40c-45bc-b66d-de439acf4384 ${ builtins.typeOf string }" ;
+        is-appended = builtins.any ( w : builtins.substring ( length - 1 ) 1 string == w ) whitespace ;
+        is-prepended = builtins.any ( w : builtins.substring 0 1 string == w ) whitespace ;
+	length = builtins.stringLength string ;
+	step =
+	  if is-appended then strip ( builtins.substring 0 ( length  - 1 ) string )
+	  else if is-prepended then strip ( builtins.substring 1 ( length - 1 ) string )
+	  else string ;
+        string = stringify expression ;
+        stringify = builtins.import ./stringify.nix ;
+        whitespace = [ " " "\n" "\t" ] ;
+        in step ;
   in strip
