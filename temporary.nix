d11c421a7bd2f7ab23b1a4b70d9abfe26ac22c62
Emory Merryman <emory.merryman@gmail.com>
2023-08-08 20:55:40 -0400

    change resource

--- temporary.nix
+++ temporary.nix
@@ -30,7 +30,7 @@
         ${ target.flock }/bin/flock 203 &&
         ${ if builtins.typeOf init == "lambda" then "${ target.coreutils }/bin/ln --symbolic ${ init shell-scripts } ${ bash-variable "TEMPORARY_DIRECTORY" }/init.sh" else "# NO INIT" } &&
         ${ if builtins.typeOf release == "lambda" then "${ target.coreutils }/bin/ln --symbolic ${ release shell-scripts } ${ bash-variable "TEMPORARY_DIRECTORY" }/release.sh" else "# NO RELEASE" } &&
-        ${ if builtins.typeOf init == "lambda" then "${ bash-variable "TEMPORARY_DIRECTORY" }/init.sh"  else "# NO INIT" } &&
+        ${ if builtins.typeOf init == "lambda" then "${ bash-variable "TEMPORARY_DIRECTORY" }/init.sh ${ bash-variable "TEMPORARY_DIRECTORY" }/resource"  else "# NO INIT" } &&
         ${ target.coreutils }/bin/echo ${ bash-variable "TEMPORARY_DIRECTORY" }
       '' ;
     scripts = builtins.import ./scripts.nix ;
