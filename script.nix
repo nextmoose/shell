42a8d466b4296402a6e337c286faeeee5b25c405
Emory Merryman <emory.merryman@gmail.com>
2023-08-28 19:08:27 -0400

    untabify

--- script.nix
+++ script.nix
@@ -20,42 +20,42 @@
       let
         isolated = { init ? builtins.null , release ? builtins.null } : resource arguments { init = init ; release = release ; salt = builtins.null ; track = builtins.null ; } ;
         penultimate =
-	  {
-	    bash-variable = bash-variable ;
-	    hash = bash-variable hash ;
-	    isolated = isolated ;
-	    shared = shared ;
-	    shell-script = shell-script ;
-	    shell-scripts = shell-scripts ;
-	    shell-script-bin = shell-script-bins ;
-	    shell-script-bins = shell-script-bins ;
-	    timestamp = bash-variable arguments.timestamp ;
-	  } ;
-	shared =
-	  let
-	    lambda = track : track.input ( { init ? builtins.null , release ? builtins.null , salt ? builtins.null } : resource arguments { init = init ; release = release ; salt = salt ; track = track ; } ) ;
-	    list = track : track.interim ;
-	    null = track : null ;
-	    set = track : track.interim ;
-	    undefined = track : track.throw "3318b42b-515d-4b9f-b210-096ea97e928c" ;
-	    in visit { lambda = lambda ; list = list ; null = null ; set = set ; undefined = undefined ; } arguments.shared ;
-	shell-script-bin = scripts : scripts shell-script-bins ;
-	shell-script-bins = inject scripts arguments ( { shell-script-bin } : shell-script-bin ) ;
-	shell-script = scripts : scripts shell-scripts ;
-	shell-scripts = inject scripts arguments ( { shell-script } : shell-script ) ;
-	ultimate = arguments // penultimate ;
+          {
+            bash-variable = bash-variable ;
+            hash = bash-variable hash ;
+            isolated = isolated ;
+            shared = shared ;
+            shell-script = shell-script ;
+            shell-scripts = shell-scripts ;
+            shell-script-bin = shell-script-bins ;
+            shell-script-bins = shell-script-bins ;
+            timestamp = bash-variable arguments.timestamp ;
+          } ;
+        shared =
+          let
+            lambda = track : track.input ( { init ? builtins.null , release ? builtins.null , salt ? builtins.null } : resource arguments { init = init ; release = release ; salt = salt ; track = track ; } ) ;
+            list = track : track.interim ;
+            null = track : null ;
+            set = track : track.interim ;
+            undefined = track : track.throw "3318b42b-515d-4b9f-b210-096ea97e928c" ;
+            in visit { lambda = lambda ; list = list ; null = null ; set = set ; undefined = undefined ; } arguments.shared ;
+        shell-script-bin = scripts : scripts shell-script-bins ;
+        shell-script-bins = inject scripts arguments ( { shell-script-bin } : shell-script-bin ) ;
+        shell-script = scripts : scripts shell-scripts ;
+        shell-scripts = inject scripts arguments ( { shell-script } : shell-script ) ;
+        ultimate = arguments // penultimate ;
         in inject lambda ultimate ;
     inject = builtins.import ./inject.nix ;
     lambda =
       let
         lambda = track : track.input ;
-	string = track : { } : track.input ;
-	undefined = track : track.throw "a1b9448a-a542-4183-9862-59cd1c658341" ;
+        string = track : { } : track.input ;
+        undefined = track : track.throw "a1b9448a-a542-4183-9862-59cd1c658341" ;
         in visit { lambda = lambda ; string = string ; undefined = undefined ; } track.input ;
     program =
       ''
-	${ strip timestamp }
-	
+        ${ strip timestamp }
+        
         ${ code }
       '' ;
     resource = builtins.import ./resource.nix ;
@@ -70,12 +70,12 @@
     timestamp =
       let
         null = track : "# UNDEFINED TIMESTAMP" ;
-	string =
-	  track :
-	    ''
-	      export ${ arguments.timestamp }=${ bash-variable "${ arguments.timestamp }:=$( ${ target.coreutils }/bin/date +%s )" }
-	    '' ;
-	undefined = track : track.throw "491ed2c7-73ed-4fe6-bee8-f6462cc0f0ca" ;
+        string =
+          track :
+            ''
+              export ${ arguments.timestamp }=${ bash-variable "${ arguments.timestamp }:=$( ${ target.coreutils }/bin/date +%s )" }
+            '' ;
+        undefined = track : track.throw "491ed2c7-73ed-4fe6-bee8-f6462cc0f0ca" ;
         in visit { null = null ; string = string ; undefined = undefined ; } arguments.timestamp ;
     try = builtins.import ./try.nix ;
     visit = builtins.import ./visit.nix ;
--- try.nix
+++ try.nix
@@ -3,6 +3,6 @@ fun :
     test =
       seed :
         let
-	  result = fun seed ;
-	  in if result.success then result.value else test ( seed + 1 ) ;
+          result = fun seed ;
+          in if result.success then result.value else test ( seed + 1 ) ;
     in test 0
